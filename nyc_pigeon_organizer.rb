64bbb1b82db4f67e540b0e9b2e91f7720f98733c
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-10 22:44:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,43 @@
-def nyc_pigeon_organizer(data)
-organised_hash = { }
+def nyc_pigeon_organizer (data)
+  final = {}
 
-data.each do | first_key, all_other|
-  all_other.each do |category, array|
-    array.each do |name|
-      organised_hash[name] = {:colour => [], :gender => [], :lives => [] }
-    end
-  end
-end
-x = organised_hash.keys
-data[:colour].each do |bird_colour, name|
-  name.each do |bird_name|
-    x.each do |item|
-      if bird_name === item 
-        organised_hash[item][:colour] << bird_colour.to_s end
-      end
-    end
-  end
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  
+  return final 
+end 
+
+nyc_pigeon_organizer(pigeon_data)
